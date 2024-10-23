c9c1827e33e253349868ea86294267905a31ef78
nsaimk <nsaimkorkmaz1@gmail.com>
2024-10-24 00:25:53 +0100

    trying: socket.io path is added

--- server/index.js
+++ server/index.js
@@ -23,22 +23,22 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        credentials: true
-    },
-});
-
-// const io = require('socket.io')(server, {
+// const io = new Server(server, {
 //     cors: {
 //         origin: '*',
 //         methods: ['GET', 'POST', 'PUT', 'DELETE'],
 //         credentials: true
 //     },
-//     path: '/socket.io'
 // });
+
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        credentials: true
+    },
+    path: '/socket.io'
+});
 
 
 
